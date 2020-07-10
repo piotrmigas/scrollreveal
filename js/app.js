2ed49d1a18c9efa0fc9cde4cca21d50597f5d3e4
piotrmigas <piotr.migas@icloud.com>
2020-07-10 18:32:38 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
-$(function() {
+$(function () {
   $("body").removeClass("fade-out");
 });
 
 window.sr = ScrollReveal();
 sr.reveal(".navbar", {
   duration: 2000,
-  origin: "bottom"
+  origin: "bottom",
 });
 sr.reveal(".showcase-left", {
   duration: 2000,
   origin: "top",
-  distance: "300px"
+  distance: "300px",
 });
 sr.reveal(".showcase-right", {
   duration: 2000,
   origin: "right",
-  distance: "300px"
+  distance: "300px",
 });
 sr.reveal(".showcase-btn", {
   duration: 2000,
   delay: 1000,
-  origin: "bottom"
+  origin: "bottom",
 });
 sr.reveal("#testimonial div", {
   duration: 2000,
-  origin: "bottom"
+  origin: "bottom",
 });
 sr.reveal(".info-left", {
   duration: 2000,
   origin: "left",
   distance: "300px",
-  viewFactor: 0.2
+  viewFactor: 0.2,
 });
 sr.reveal(".info-right", {
   duration: 2000,
   origin: "right",
   distance: "300px",
-  viewFactor: 0.2
+  viewFactor: 0.2,
 });
